1a5018d7a9db8f1be0a0761b0ca0d2cebbb40ab1
Nivas Chaitanya K <nivaschaitanya9@gmail.com>
2026-09-08 19:39:12 +0530

    Time: 8 ms (8.47%) | Memory: 42.9 MB (8.32%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.java
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -1,7 +1,7 @@
 class Solution {
     public int countCommas(int n) {
         int count = 0;
-
+        //Nivas
         for (int i = 1; i <= n; i++) {
             if (i >= 1000) {
                 count++;
